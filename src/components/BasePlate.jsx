cc61c4c632d915144dc3c71e6534be95efb00471
markus <markusteumel@gmail.com>
2024-07-11 09:01:55 +0200

    added Baseplate

---
+++ src/components/BasePlate.jsx
@@ -0,0 +1,14 @@
+import { useRef } from "react";
+
+export default function BasePlate({args, color}) {
+    const mesh = useRef()
+
+
+
+    return (
+        <mesh ref={mesh} position={[0, -0.5, 15]}>
+          <boxGeometry args={args} />
+          <meshStandardMaterial color={color} />
+        </mesh>
+      );
+}
