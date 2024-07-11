c2af4cc08d0afc493f7214e941cf5a8c65b335d4
Dasajton <david.sajitz.94@gmail.com>
2024-07-11 10:35:49 +0200

    enemy created and is moving towards z axis

--- src/components/Enemy.jsx
+++ src/components/Enemy.jsx
@@ -3,25 +3,31 @@ import { useEffect, useState, useRef } from "react";
 import { useGLTF, useAnimations } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-
 export default function Enemy() {
+  const { animations } = useGLTF("./oilBarrel.glb");
+  const gltf = useLoader(GLTFLoader, "./oilBarrel.glb");
+  const [velocity, setVelocity] = useState({ x: 0, y: 0, z: 0 });
 
-    const gltf = useLoader(GLTFLoader, "./oilBarrel.glb")
-  
-  
-  
-  
-  
-  
-  
-    return (
+  const { ref, actions, names } = useAnimations(animations);
+
+  useFrame(() => {
+    let newVelocity = { ...velocity };
+    newVelocity.z = 0.05;
+
+    ref.current.position.z += newVelocity.z;
+
+    setVelocity(newVelocity);
+  });
+
+  return (
     <>
-        <primitive
-            object={gltf.scene}
-            scale={0.5}
-            rotation-z={1.57}
-            position-y={0.35}
-        />
+      <primitive
+        object={gltf.scene}
+        scale={0.5}
+        rotation-z={1.57}
+        position-y={0.35}
+        ref={ref}
+      />
     </>
-  )
+  );
 }
