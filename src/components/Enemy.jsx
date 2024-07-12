5e2528954e7667e59240c2dd434c509d3bc559b2
markus <markusteumel@gmail.com>
2024-07-12 11:14:23 +0200

    added detection now understanding it next

--- src/components/Enemy.jsx
+++ src/components/Enemy.jsx
@@ -1,12 +1,44 @@
+// import { useGLTF } from "@react-three/drei";
+// import { useFrame } from "@react-three/fiber";
+// import { useMemo, useRef } from "react";
+
+// export default function Enemy({ positionX }) {
+//   const model = useGLTF("./oilBarrel.glb");
+//   const scene = useMemo(() => {
+//     return model.scene.clone();
+//   }, [model]);
+//   const ref = useRef();
+//   const velocity = 0.05;
+
+//   useFrame(() => {
+//     if (ref.current) {
+//       ref.current.position.z += velocity;
+//     }
+//   });
+
+//   return (
+//     <primitive
+//       object={scene}
+//       scale={0.5}
+//       rotation-z={1.57}
+//       position={[positionX, 0.35, -20]}
+//       ref={ref}
+//     />
+//   );
+// }
 import { useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useMemo, useRef } from "react";
+import * as THREE from 'three';
 
 export default function Enemy({ positionX }) {
   const model = useGLTF("./oilBarrel.glb");
   const scene = useMemo(() => {
-    return model.scene.clone();
+    const clonedScene = model.scene.clone();
+    clonedScene.userData = { type: 'enemy' };
+    return clonedScene;
   }, [model]);
+
   const ref = useRef();
   const velocity = 0.05;
 
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,145 @@
+// import { useFrame } from "@react-three/fiber";
+// import { useEffect, useState } from "react";
+// import { useGLTF, useAnimations } from "@react-three/drei";
+
+// export default function Player({ position }) {
+//   const { scene, animations } = useGLTF("/ReadiedAsset.glb");
+//   const { ref, actions, names } = useAnimations(animations);
+
+//   const [velocity, setVelocity] = useState({ x: 0, y: 0, z: 0 });
+//   const [keysPressed, setKeysPressed] = useState({
+//     forward: false,
+//     backward: false,
+//     leftward: false,
+//     rightward: false,
+//     jump: false,
+//   });
+
+//   const [index] = useState(0);
+
+//   useEffect(() => {
+//     const handleKeyDown = (event) => {
+//       switch (event.code) {
+//         case "ArrowUp":
+//         case "KeyW":
+//           setKeysPressed((prev) => ({ ...prev, forward: true }));
+//           actions[names[1]].play();
+//           break;
+//         case "ArrowDown":
+//         case "KeyS":
+//           setKeysPressed((prev) => ({ ...prev, backward: true }));
+//           actions[names[1]].play();
+//           break;
+//         case "ArrowLeft":
+//         case "KeyA":
+//           setKeysPressed((prev) => ({ ...prev, leftward: true }));
+//           actions[names[1]].play();
+//           break;
+//         case "ArrowRight":
+//         case "KeyD":
+//           setKeysPressed((prev) => ({ ...prev, rightward: true }));
+//           actions[names[1]].play();
+//           break;
+//         case "Space":
+//           setKeysPressed((prev) => ({ ...prev, jump: true }));
+//           actions[names[2]].play();
+//           break;
+//         default:
+//           break;
+//       }
+//     };
+
+//     const handleKeyUp = (event) => {
+//       switch (event.code) {
+//         case "ArrowUp":
+//         case "KeyW":
+//           setKeysPressed((prev) => ({ ...prev, forward: false }));
+//           actions[names[1]].stop();
+//           break;
+//         case "ArrowDown":
+//         case "KeyS":
+//           setKeysPressed((prev) => ({ ...prev, backward: false }));
+//           actions[names[1]].stop();
+//           break;
+//         case "ArrowLeft":
+//         case "KeyA":
+//           setKeysPressed((prev) => ({ ...prev, leftward: false }));
+//           actions[names[1]].stop();
+//           break;
+//         case "ArrowRight":
+//         case "KeyD":
+//           setKeysPressed((prev) => ({ ...prev, rightward: false }));
+//           actions[names[1]].stop();
+//           break;
+//         case "Space":
+//           setKeysPressed((prev) => ({ ...prev, jump: false }));
+//           actions[names[2]].stop();
+//           break;
+//         default:
+//           break;
+//       }
+//     };
+
+//     window.addEventListener("keydown", handleKeyDown);
+//     window.addEventListener("keyup", handleKeyUp);
+
+//     return () => {
+//       window.removeEventListener("keydown", handleKeyDown);
+//       window.removeEventListener("keyup", handleKeyUp);
+//     };
+//   }, [actions, names, index]);
+
+//   useFrame(() => {
+//     let newVelocity = { ...velocity };
+
+//     if (keysPressed.forward) newVelocity.z = -0.05;
+//     if (keysPressed.backward) newVelocity.z = 0.05;
+//     if (keysPressed.leftward) newVelocity.x = -0.05;
+//     if (keysPressed.rightward) newVelocity.x = 0.05;
+//     if (keysPressed.jump && !newVelocity.isJumping) {
+//       newVelocity.y = 0.1;
+//       newVelocity.isJumping = true;
+//     }
+
+//     newVelocity.y -= 0.005;
+
+//     if (ref.current) {
+//       ref.current.position.x += newVelocity.x;
+//       ref.current.position.y += newVelocity.y;
+//       ref.current.position.z += newVelocity.z;
+
+//       newVelocity.x = 0;
+//       newVelocity.z = 0;
+
+//       if (ref.current.position.y < 0) {
+//         ref.current.position.y = 0;
+//         newVelocity.y = 0;
+//         newVelocity.isJumping = false;
+//       }
+//     }
+
+//     setVelocity(newVelocity);
+//   });
+
+//   useEffect(() => {
+//     actions[names[index]].play();
+//     return () => actions[names[index]].stop();
+//   }, [index, actions, names]);
+
+//   return (
+//     <primitive
+//       rotation-y={3}
+//       object={scene}
+//       ref={ref}
+//       scale={1}
+//       position={position}
+//     />
+//   );
+// }
 import { useFrame } from "@react-three/fiber";
-import { useEffect, useState } from "react";
-import { useGLTF, useAnimations } from "@react-three/drei";
+import { useEffect, useState, useRef } from "react";
+import { useGLTF, useAnimations, meshBounds } from "@react-three/drei";
+import * as THREE from 'three';
 
 export default function Player({ position }) {
   const { scene, animations } = useGLTF("/ReadiedAsset.glb");
@@ -15,9 +154,12 @@ export default function Player({ position }) {
     jump: false,
   });
 
-  const [index] = useState(0);
+  const playerBox = useRef();
+  const enemyBox = useRef(new THREE.Box3().setFromObject(new THREE.Object3D()));
 
   useEffect(() => {
+    playerBox.current = new THREE.Box3().setFromObject(ref.current);
+
     const handleKeyDown = (event) => {
       switch (event.code) {
         case "ArrowUp":
@@ -87,9 +229,9 @@ export default function Player({ position }) {
       window.removeEventListener("keydown", handleKeyDown);
       window.removeEventListener("keyup", handleKeyUp);
     };
-  }, [actions, names, index]);
+  }, [actions, names]);
 
-  useFrame(() => {
+  useFrame(({ scene }) => {
     let newVelocity = { ...velocity };
 
     if (keysPressed.forward) newVelocity.z = -0.05;
@@ -116,15 +258,25 @@ export default function Player({ position }) {
         newVelocity.y = 0;
         newVelocity.isJumping = false;
       }
+
+      playerBox.current.setFromObject(ref.current);
+      scene.traverse((object) => {
+        if (object.userData.type === 'enemy') {
+          enemyBox.current.setFromObject(object);
+          if (playerBox.current.intersectsBox(enemyBox.current)) {
+            console.log('Collision detected!');
+          }
+        }
+      });
     }
 
     setVelocity(newVelocity);
   });
 
   useEffect(() => {
-    actions[names[index]].play();
-    return () => actions[names[index]].stop();
-  }, [index, actions, names]);
+    actions[names[0]].play();
+    return () => actions[names[0]].stop();
+  }, [actions, names]);
 
   return (
     <primitive
@@ -133,6 +285,8 @@ export default function Player({ position }) {
       ref={ref}
       scale={1}
       position={position}
+      raycast={meshBounds}
     />
   );
 }
+
